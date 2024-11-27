9eb4416a80b8a47230169832c2eccf43adf27da5
basemw0 <basemwalid44@gmail.com>
2024-11-27 18:20:54 +0200

    Update Person.java

--- SourceCode/Person.java
+++ SourceCode/Person.java
@@ -5,7 +5,15 @@ public abstract class Person {
     protected String password;
     protected Date DOB;
 
+Person(){
+   username=NULL;
+   password=NULL;
+}
 
-
-
+Person(String user, String pass, int year, int month, int day){
+   this.username=user;
+   this.password=pass;
+   DOB = new Date(year,month,day);
+}
+   
 }
