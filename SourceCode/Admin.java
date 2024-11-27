c161f3dc2c81a8c39e70e5f015cf7709980c1227
adhamthearray <dommawalid@gmail.com>
2024-11-27 19:52:05 +0200

    Update Admin.java

--- SourceCode/Admin.java
+++ SourceCode/Admin.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+import java.util.Date;
 public class Admin extends Person
 {
+    Scanner input = new Scanner(System.in);
     String[] role = {"ceo","call"};
     private String Role;
     private String working_hours;
 
+    public Admin(){
+        Person();
+        Role="null";
+        working_hours="null";
+
+    }
+    public Admin(String user, String pass, int year, int month, int day,String role,String WH){
+        Person(user,pass,year,month,day);
+        this.Role=role;
+        this.working_hours=WH;
+
+    }
     public String getRole() {
         return Role;
     }
@@ -24,10 +39,90 @@ public class Admin extends Person
     public void setWorking_hours(String working_hours) {
         this.working_hours = working_hours;
     }
-    public void add_supplier(Supplier new_sup){}
-    public void view_products(){}
-    public void view_suppliers(){}
-    public void view_orders(){}
-    public void view_customers(){}
+    public void add_supplier(Supplier new_sup){
+        Database.suppliers.add(new_sup);
+        System.out.println(new_sup.COMM_NAME +"  has been added :)");
+    }
+    public void view_products(){
+        System.out.println("ALL PRODUCTS:");
+        System.out.println(" ");
+
+
+
+        for(int i=0;i<Database.suppliers.size();i++)
+        {
+            Supplier supplier = Database.suppliers.get(i);
+            System.out.println("*"+supplier.COMM_NAME+": ");
+            if ( supplier.SUPP_PROD.length > 0) {
+                for (int j = 0; j < supplier.SUPP_PROD.length; j++) {
+                    int count = j + 1;
+                    System.out.println(count + "-" + supplier.SUPP_PROD[j].getName()+" Price:"+supplier.SUPP_PROD[j].getPrice());
+                }
+            } else {
+                System.out.println("No products available");
+            }
+        }
+
+
+    }
+    public void view_suppliers(){
+        if (Database.suppliers.isEmpty()) {
+            System.out.println("No suppliers available");
+            return;
+        }
+        System.out.println("ALL SUPPLIERS:");
+        System.out.println(" ");
+        for(int i=0;i<Database.suppliers.size();i++)
+        {
+            Supplier supplier = Database.suppliers.get(i);
+            int count=i+1;
+            System.out.println(count + "-"+ supplier.COMM_NAME);
+
+        }
+    }
+    public void view_orders(){
+        System.out.println("ALL ORDERS:");
+        System.out.println(" ");
+
+
+
+        for(int i=0;i<Database.users.size();i++)
+        {
+            Customer customer = Database.users.get(i);
+            System.out.println("*"+customer.username+": ");
+            if ( customer.orders.size() > 0) {
+                for (int j = 0; j < customer.orders.size(); j++) {
+
+                    Order order=customer.orders.get(j);
+                    int count=j+1;
+                    System.out.print("Order " + count+" :");
+                    order.viewOrder();
+                    System.out.println("___________________________");
+                    System.out.println(" ");
+
+
+                }
+            } else {
+                System.out.println("No Orders Done");
+            }
+        }
+    }
+    public void view_customers(){
+        if (Database.users.isEmpty()) {
+            System.out.println("No Customers available");
+            return;
+        }
+        System.out.println("ALL Customers:");
+        System.out.println(" ");
+
+        for(int i=0;i<Database.users.size();i++)
+        {
+            Customer customer = Database.users.get(i);
+
+            int count=i+1;
+            System.out.println(count + "-"+ customer.username);
+
+        }
+    }
 
 }
