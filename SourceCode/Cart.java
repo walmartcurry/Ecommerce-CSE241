9af6c2f11cf24f335e2df44bbed1f11041ed1c19
basemw0 <basemwalid44@gmail.com>
2024-11-27 18:08:55 +0200

    Update Cart.java

--- SourceCode/Cart.java
+++ SourceCode/Cart.java
@@ -1,22 +1,49 @@
 import java.util.ArrayList;
+
 public class Cart{
+
     ArrayList<Product> cartItems;
     private double totalPrice;
-    Cart(){}
-    public void calcTotalPrice(){}
+
+    Cart(){
+        totalPrice = 0;
+        cartItems = new ArrayList<Product>();
+    }
+
+    public void calcTotalPrice(){
+        for (Product cartItem : cartItems) {
+            this.totalPrice += cartItem.getPrice();
+        }
+    }
+
     public double getTotalPrice() {
         return totalPrice;
     }
-    public void viewcart(){};
-    public void AddToCart(){};//7aneb2a n4oof
-    public void removeFromCart(){}; //7aneb2a n4oof
-    public void IncreaseProd(){}; // Increases quantity of product using Product.increaseQuantity
-    public void DecreaseProd(){}; // Decreases quantity of product using Product.decreaseQuantity
 
-    
+    public void viewcart(){
+        for (Product cartItem : cartItems) {
+            System.out.println(cartItem.getName());
+        }
+    };
 
-
-
-
-
+    public void AddToCart(Product ADD){
+        cartItems.add(ADD);
+    };
+    public void removeFromCart(Product Remove){
+        cartItems.remove(Remove);
+    };
+    public void IncreaseProd(Product inc){
+        for (Product cartItem : cartItems) {
+            if (cartItem==inc){
+                cartItem.setQuantity(cartItem.getQuantity()+1);
+            }
+        }
+    }; // Increases quantity of product using Product.increaseQuantity
+    public void DecreaseProd(Product dec){
+        for (Product cartItem : cartItems) {
+            if (cartItem==dec){
+                cartItem.setQuantity(cartItem.getQuantity()-1);
+            }
+        }
+    }; // Decreases quantity of product using Product.decreaseQuantity
 }
