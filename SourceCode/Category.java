997d2307ccd6450aa733003ac5267fb12f74483e
walmartcurry <amirtamer31@gmail.com>
2024-11-27 13:35:41 +0200

    Update Category.java

--- SourceCode/Category.java
+++ SourceCode/Category.java
@@ -1,6 +1,7 @@
 public class Category {
     private String[] categories = {"Dairy","Electronics"};//Add more categories
     protected String Category;
-    Category(String category){}; //recieves a category string checks if it is in the array and sets Category to it if so.
-
+    Category(String category){};
+    bool checkValidity(String category);
+    
 }
