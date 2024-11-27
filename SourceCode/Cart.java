46276f6f263d0d8eaf2dee5c2ece1b2cb83a14d1
basemw0 <basemwalid44@gmail.com>
2024-11-27 14:35:16 +0200

    Update Cart.java

--- SourceCode/Cart.java
+++ SourceCode/Cart.java
@@ -7,6 +7,7 @@ public class Cart{
     public double getTotalPrice() {
         return totalPrice;
     }
+    public void viewcart(){};
     public void AddToCart(){};//7aneb2a n4oof
     public void removeFromCart(){}; //7aneb2a n4oof
 
