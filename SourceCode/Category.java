6c29bdb0f7349d48c933eaafa915082e040f673b
weloo11 <yousefwa2005@gmail.com>
2024-11-27 21:10:35 +0200

    Update Category.java

--- SourceCode/Category.java
+++ SourceCode/Category.java
@@ -1,7 +1,36 @@
-public class Category {
-    private static String[] categories = {"Dairy","Electronics","Other"};//Add more categories
-    protected String Category;
-    Category(String category){};
-    public static bool checkValidity(String category);
-    
+
+public class Category{
+    private static final String[] categories={"electronics", "bakery", "clothes", "laptops", "others", 
+        "furniture", "toys", "books", "groceries", "sports"};
+    private String type;
+    public Category(){
+        this.type="others";
+    }
+    public String gettype()
+    {
+        return type;
+    }
+
+    public Category(String category)
+    {
+        category=category.toLowerCase();
+        if(checkValidity(category))
+        {
+            this.type=category;
+        }
+        else {
+            this.type="others";
+        }
+    }
+    private boolean checkValidity(String category)
+    {
+        for(int i=0;i< categories.length;i++)
+        {
+            if(category.equals(categories[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
