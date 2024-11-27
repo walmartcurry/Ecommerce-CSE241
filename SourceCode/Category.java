3dc37007bbe01ff1ef60642403a8fd0b2ad19eec
walmartcurry <amirtamer31@gmail.com>
2024-11-27 13:47:06 +0200

    Update Category.java

--- SourceCode/Category.java
+++ SourceCode/Category.java
@@ -1,7 +1,7 @@
 public class Category {
-    private String[] categories = {"Dairy","Electronics","Other"};//Add more categories
+    private static String[] categories = {"Dairy","Electronics","Other"};//Add more categories
     protected String Category;
     Category(String category){};
-    bool checkValidity(String category);
+    public static bool checkValidity(String category);
     
 }
