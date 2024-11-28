ee4c40b2cd592a1ca67fcf5bf4c80ef45ef7e232
walmartcurry <amirtamer31@gmail.com>
2024-11-28 21:37:36 +0200

    Update Product.java

--- SourceCode/Product.java
+++ SourceCode/Product.java
@@ -1,55 +1,40 @@
-public class Product {
-        private String name;
-        private int quantity=0;
-        private int stock=0;
-        private Category mycat;
-        private double price;
-        Product(){
-            this.name="null";
-            this.stock=0;
-            this.price=0.0;
-            mycat=new Category();
+public abstract class Product {
+    protected String name;
+    protected Category mycat;
+    protected double price;
+    Product(){
+        this.name="null";
+        this.price=0.0;
+        mycat=new Category();
 
-        } // Set defualt to "NULL" and 0.0 and "Other"
-        Product(String name , double price, String category,int stock){
-            this.name=name;
-            this.price=price;
-            this.stock=stock;
-            mycat=new Category(category);
-        }
+    } 
+    Product(String name , double price, String category){
+        this.name=name;
+        this.price=price;
+        mycat=new Category(category);
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setPrice(double price) {
-            this.price = price;
-        }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 
-        public String getName() {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public double getPrice() {
-            return price;
-        }
+    public double getPrice() {
+        return price;
+    }
+    public String getMycat() {
+        return mycat.gettype();
+    }
+}
 
-        public int getStock() {
-            return stock;
-        }
-
-        public void setStock(int stock) {
-            this.stock = stock;
-        }
-
-        public void increaseStock(int new_stock_quantity){
-            this.stock=this.stock+new_stock_quantity;
-            System.out.println("Now,There are "+this.stock +" items of "+this.name+" available for Sale!");
-        };
-        public void increaseQuantity(){
-            quantity++;
-        }; // Increases quantity while it's less than stock
 
 
     }
