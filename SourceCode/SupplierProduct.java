3b0d2ef5752efe65eec9577794a1124032cfaaf8
walmartcurry <amirtamer31@gmail.com>
2024-11-28 21:43:37 +0200

    Update SupplierProduct.java

--- SourceCode/SupplierProduct.java
+++ SourceCode/SupplierProduct.java
@@ -19,6 +19,7 @@ public class SupplierProduct extends Product {
     public Supplier getSupplier() {
         return supplier;
     }
+    @Override
     public void viewProduct(){
         System.out.println("Product Name :" + name);
         System.out.println("Price :" + price);
