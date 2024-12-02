297b6c9f80c01c62ce18f3f1175240036fa61904
walmartcurry <amirtamer31@gmail.com>
2024-12-02 19:18:00 +0200

    Update Person.java

--- SourceCode 2.0/Person.java
+++ SourceCode 2.0/Person.java
@@ -9,10 +9,20 @@ Person(){
    password="NULL";
 }
 
-Person(String user, String pass, int year, int month, int day){
+Person(String user, String pass, int day, int month, int year){
    this.username=user;
    this.password=pass;
-   DOB = LocalDate.of(year,month,day);
+   DOB = 
+   LocalDate.of(year,month,day);
+}
+public void setUsername(String username) {
+    this.username = username;
+}
+public String getUsername() {
+    return username;
+}
+public void setPassword(String password) {
+    this.password = password;
 }
    
 }
