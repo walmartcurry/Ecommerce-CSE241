03bc578a1861858c27b6aa0de1da8c2b45f8970f
walmartcurry <amirtamer31@gmail.com>
2024-12-02 19:18:40 +0200

    Update Database.java

--- SourceCode 2.0/Database.java
+++ SourceCode 2.0/Database.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public abstract class Database {
-    static ArrayList<Supplier> suppliers;
-    static ArrayList<Customer> users;
-    static ArrayList<Order> orders;
-    static ArrayList<Admin> admins;
+    static ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
+    static ArrayList<Customer> users = new ArrayList<Customer>();
+    static ArrayList<Order> orders = new ArrayList<Order>();
+    static ArrayList<Admin> admins = new ArrayList<Admin>();
 }
