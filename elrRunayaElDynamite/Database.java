f378c302b569639aa624dbc27b16a2c3b5b91be3
walmartcurry <amirtamer31@gmail.com>
2024-12-06 00:51:45 +0200

    Update Database.java

--- elrRunayaElDynamite/Database.java
+++ elrRunayaElDynamite/Database.java
@@ -4,4 +4,5 @@ public abstract class Database {
     static ArrayList<Customer> users = new ArrayList<Customer>();
     static ArrayList<Order> orders = new ArrayList<Order>();
     static ArrayList<Admin> admins = new ArrayList<Admin>();
+    static ArrayList<Supplier> supplierRequests = new ArrayList<Supplier>();
 }
