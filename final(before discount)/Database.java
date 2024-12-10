58e82c237899fbb784523103b237bb749c370955
walmartcurry <amirtamer31@gmail.com>
2024-12-10 19:18:40 +0200

    Delete final(before discount) directory

--- final(before discount)/Database.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-public abstract class Database {
-    static ArrayList<Supplier> suppliers;
-    static ArrayList<Customer> users;
-    static ArrayList<Order> orders;
-    static ArrayList<Admin> admins;
-}
